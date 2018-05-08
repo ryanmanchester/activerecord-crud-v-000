ca13391764a656987543e28d3cb5732b799263d5
Ryan Manchester <ryanm17@gmail.com>
2018-05-08 16:02:25 +0000

    automatically backed up by learn

--- db/migrate/20180508155906_create_movies.rb
+++ db/migrate/20180508155906_create_movies.rb
@@ -2,10 +2,10 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.string :title
-      t.string :release_date
+      t.integer :release_date
       t.string :director
       t.string :lead
-      t.datetime :in_theaters
+      t.boolean :in_theaters
     end
   end
 end
